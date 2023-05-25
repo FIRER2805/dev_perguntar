099dc52d29cfb74f62212cdeb500a7d2f276a83c
FIRER2805 <gabriel_henrique2805@hotmai.com>
2023-05-25 09:19:39 -0300

    banco e vo's

--- source/model/Banco.java
+++ source/model/Banco.java
@@ -2,7 +2,10 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Banco {
 	public static final String NOME_BANCO = "db_dev_perguntar";
@@ -10,6 +13,13 @@ public class Banco {
 	public static final String SENHA = "admin";
 	public static final String URL = "jdbc:mysql://localhost:3306/" + NOME_BANCO;
 	
+	
+	/** 
+	 * este método estabelece uma conexão com o banco
+	 * 
+	 * @return objeto Connection
+	 * @author Gabriel Henrique
+	 * */
 	public static Connection getConnection()
 	{
 		Connection retorno = null;
@@ -23,5 +33,82 @@ public class Banco {
 			System.out.println("Erro ao buscar conexao com o banco");
 		}
 		return retorno;
+	}
+	
+	
+	/** 
+	 * gera um statement para executar querys estaticas
+	 * 
+	 * @param conexão
+	 * @return objeto Statement
+	 * @author Gabriel Henrique
+	 */
+	public static Statement getStatement(Connection c) 
+	{
+		Statement stmt = null;
+		try {
+			stmt = c.createStatement();
+		} catch (SQLException e) {
+			stmt = null;
+			System.out.println("Erro no getStatement na classe Banco");
+		}
+		return stmt;
+	}
+	
+	public static PreparedStatement getPreparedStmt(Connection c, String s)
+	{
+		PreparedStatement pStmt = null;
+		try 
+		{
+			pStmt = c.prepareStatement(s);
+		}
+		catch(SQLException e)
+		{
+			pStmt = null;
+			System.out.println("Erro no getPreparedStmt na classe Banco");
+		}
+		return pStmt;
+	}
+	
+	public static PreparedStatement getPreparedStmtPK(Connection c, String s)
+	{
+		PreparedStatement pStmt = null;
+		try 
+		{
+			pStmt = c.prepareStatement(s, PreparedStatement.RETURN_GENERATED_KEYS);
+		}
+		catch(SQLException e)
+		{
+			pStmt = null;
+			System.out.println("Erro no getPreparedStmtPK na classe Banco");
+		}
+		return pStmt;
+	}
+	
+	public static void closeConnection(Connection c)
+	{
+		try {
+			c.close();
+		} catch (SQLException e) {
+			System.out.println("Erro no closeConnection na classe Banco");
+		}
+	}
+	
+	public static void closeStatement(Statement s)
+	{
+		try {
+			s.close();
+		} catch (SQLException e) {
+			System.out.println("Erro no closeStatement na classe Banco");
+		}
+	}
+	
+	public static void closeResultSet(ResultSet r)
+	{
+		try {
+			r.close();
+		} catch (SQLException e) {
+			System.out.println("Erro no closeResultSet na classe Banco");
+		}
 	}
 }
---
+++ source/model/vo/Pergunta.java
@@ -0,0 +1,56 @@
+package model.vo;
+
+import java.time.LocalDateTime;
+
+public class Pergunta {
+	private int id;
+	private String titulo;
+	private String conteudo;
+	private LocalDateTime data;
+	private LocalDateTime dataResolução;
+	private Usuario usuario;
+	private Categoria categoria;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	public LocalDateTime getDataResolução() {
+		return dataResolução;
+	}
+	public void setDataResolução(LocalDateTime dataResolução) {
+		this.dataResolução = dataResolução;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
