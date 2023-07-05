f079804d06d763d7285d2641eacf7083dd773232
Gabriel <gabriel_henrique2805@hotmail.com>
2023-07-05 12:03:02 -0300

    filtro da busca de usuario

--- src/main/java/model/vo/PesquisaUsuario.java
+++ src/main/java/model/vo/PesquisaUsuario.java
@@ -55,8 +55,48 @@ public class PesquisaUsuario {
 		this.ordemPesquisa = ordemPesquisa;
 	}
 
-	
+	public boolean temFiltros()
+	{
+		if(this.isTemPergunta())
+			return true;
+		if(this.isTemresposta())
+			return true;
+		if(this.isTemSolucao())
+			return true;
+		
+		return false;
+	}
 
-	
+	public String criaFiltro()
+	{
+		String retorno = "having ";
+		boolean primeiro = true;
+		
+		if(this.isTemPergunta())
+		{
+			if(!primeiro)
+				retorno += "and ";
+			retorno += "(select count(p.id) from pergunta p where p.id_usuario = u.id) > 0 ";
+			primeiro = false;
+		}
+		
+		if(this.isTemresposta())
+		{
+			if(!primeiro)
+				retorno += "and ";
+			retorno += "(select count(r.id) from resposta r where r.id_usuario = u.id) > 0 ";
+			primeiro = false;
+		}
+		
+		if(this.isTemSolucao())
+		{
+			if(!primeiro)
+				retorno += "and ";
+			retorno += "(select count(r.id) from resposta r where r.id_usuario = u.id and r.solucao = 1) > 0 ";
+			primeiro = false;
+		}
+		
+		return retorno;
+	}
 
 }
